b98c4cc524fa7bdd3f6a0eb34fa62e24df749b4e
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-08-18 10:36:15 +0530

    Initial commit for file: note_taking_data.js

---
+++ note_taking_data.js
@@ -0,0 +1,1 @@
+export const item = {"subjects": [{"subject_name": "material", "id": "3085e02c-d715-436b-9e06-fffe83f95a73"}, {"subject_name": "all", "id": "92a34f85-7d00-49b9-90fb-06ae7e78b6ae"}, {"subject_name": "these", "id": "bcd8941e-f096-4aaa-a482-63f9bdc707f5"}], "id": "35d59df3-5ef4-41c8-980b-87a90d234517"}
